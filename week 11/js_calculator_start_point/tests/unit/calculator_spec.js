54f9c3e8a4ca4d25ee3a570432985836d9f7b33f
Dave Campbell <dave@digetl.co.uk>
2020-05-30 19:56:31 +0100

    3 tests passed

---
+++ week 11/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,32 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('should add 2 numbers', function() {
+
+    calculator.numberClick(1);
+    calculator.operatorClick("+");
+    calculator.numberClick(4);
+    calculator.operatorClick("=");
+    assert.strictEqual(5, calculator.runningTotal);
+  })
+
+  it('should subtract 1 numbner from another', function() {
+
+    calculator.numberClick(7);
+    calculator.operatorClick("-");
+    calculator.numberClick(4);
+    calculator.operatorClick("=");
+    assert.strictEqual(3, calculator.runningTotal);
+  })
+
+});
