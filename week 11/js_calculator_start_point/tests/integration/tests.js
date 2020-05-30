5ed98e2d90153364b3af312c000864ab385c156c
Dave Campbell <dave@digetl.co.uk>
2020-05-30 23:30:48 +0100

    2 passed int test

--- week 11/js_calculator_start_point/tests/integration/tests.js
+++ week 11/js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,12 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should update the display of the runnnig total after pressing the number buttons', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('273')
+  })
+
 });
