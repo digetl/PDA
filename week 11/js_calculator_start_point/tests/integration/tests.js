f34ace849101dfd60084b632c808013c87fe413e
Dave Campbell <dave@digetl.co.uk>
2020-05-30 23:37:04 +0100

    4 passed int test

--- week 11/js_calculator_start_point/tests/integration/tests.js
+++ week 11/js_calculator_start_point/tests/integration/tests.js
@@ -32,4 +32,15 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('9')
   })
+
+  it('should chain mutliple operations together', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('18')
+  })
 });
