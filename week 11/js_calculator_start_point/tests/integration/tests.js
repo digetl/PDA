94c2580ec77bad39d85fee3a7cf88a096af3c69f
Dave Campbell <dave@digetl.co.uk>
2020-05-31 10:28:56 +0100

    9 tests passed

--- week 11/js_calculator_start_point/tests/integration/tests.js
+++ week 11/js_calculator_start_point/tests/integration/tests.js
@@ -85,4 +85,13 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('3.5')
   })
+
+  it('should handle divide by 0 by displaying "error"', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('Error')
+  })
 });
